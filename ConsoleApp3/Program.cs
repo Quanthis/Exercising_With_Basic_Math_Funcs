e22a17612392228e4c968b056000efb656c554f8
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-16 16:44:07 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.WriteLine("Tell me  x (greater than 0)");
+            x = Convert.ToDouble(Console.ReadLine());
+            y = Math.Sqrt(Math.Abs(Math.Sin(x)) * Math.Log(x, 2.0));
+            Console.WriteLine(y);
+            Console.ReadKey();
+
+        }
+    }
+}
